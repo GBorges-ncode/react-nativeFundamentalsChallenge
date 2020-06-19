c9887a2289169bd89b10b6593bee050dbcc26d6f
GBorges-ncode <gilbertbdev@gmail.com>
2020-06-18 23:07:01 -0300

    Implemented Cart Functionalities

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -25,14 +25,19 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const totalValue = products
+      .map(v => v.price * v.quantity)
+      .reduce((sum, current) => sum + current, 0);
 
-    return formatValue(0);
+    return formatValue(totalValue);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalQuantity = products
+      .map(v => v.quantity)
+      .reduce((sum, current) => sum + current, 0);
 
-    return 0;
+    return totalQuantity;
   }, [products]);
 
   return (
